6e015ae33519510ce9032a7a1ad7d39912999daa
kilkoon <gluestar@naver.com>
2018-08-02 10:15:55 +0900

    update

--- Study_Algorithm_180731/Study_Algorithm_180731/Triangle.cpp
+++ Study_Algorithm_180731/Study_Algorithm_180731/Triangle.cpp
@@ -16,13 +16,24 @@ int solution(vector<int> &A)
 	size_t inputSize = A.size();
 	sort(A.begin(), A.end(), greater<int>());
 
+	if (inputSize < 3)
+		return 0;
+
 	for (size_t i = 0; i < inputSize-2; ++i)
 	{
+		if (A[i] < 0)
+			continue;
 		for (size_t j = inputSize - 1; j > i+1; --j)
 		{
+			if (A[j] < 0)
+				continue;
 			for (size_t k = j-1; k > i; --k)
 			{
+				if (A[k] < 0)
+					continue;
 				if (A[i] < A[j] + A[k])
+					return 1;
+				else if (A[i] == A[k] && A[k] == A[j])
 					return 1;
 			}
 		}
