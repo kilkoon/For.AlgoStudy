9e52f70e24c45a4d833ed6676ffe74da9ed8d3dc
kilkoon <gluestar@naver.com>
2018-08-10 03:05:08 +0900

    update

--- Study_Algorithm_180731/Study_Algorithm_180731/MaxDoubleSliceSum.cpp
+++ Study_Algorithm_180731/Study_Algorithm_180731/MaxDoubleSliceSum.cpp
@@ -7,35 +7,44 @@ int solution(vector<int> &A)
 {
 	size_t inputSize = A.size();
 
-	int max_ending = A[1];
+	int max_ending = 0;
 	int max_slice = 0;
-	int preSlice = 0;
+	int preMaxSlice = 0;
 
-	bool bMax = false;
-	size_t BeginIdx = 0;
-	size_t EndIdx = 0;
+	int smallNum= A[1];
+	int smallBuffer = 0;
 
 	for (size_t i = 1; i < inputSize - 1; ++i)
 	{
-		if (bMax && max(0, max_ending + A[i]) == max_ending + A[i])
-		{
-			EndIdx = i;
-		}
+		if (A[i] < smallBuffer)
+			smallBuffer = A[i];
+
 		max_ending = max(0, max_ending + A[i]);
 		max_slice = max(max_slice, max_ending);
 
-		if (preSlice != max_slice)
-		{
-			preSlice = max_slice;
-			BeginIdx = i;
-			bMax = true;
-		}
+		if (preMaxSlice != max_slice)
+			smallNum = smallBuffer;
+		else
+			smallBuffer = A[i];
+
+		preMaxSlice = max_slice;
 	}
 
-	int small = A[BeginIdx];
+	return max_slice - smallNum;
+}
 
-	for (size_t i = BeginIdx; i < EndIdx + 1; ++i)
-		small = min(small, A[i]);
+int solution(vector<int> &A)
+{
+	size_t inputSize = A.size();
 
-	return max_slice - small;
+	int max_ending = 0;
+	int max_slice = 0;
+
+	for (size_t i = 1; i < inputSize - 1; ++i)
+	{
+		max_ending = max(0, max_ending + A[i]);
+		max_slice = max(max_slice, max_ending);
+	}
+
+	return max_slice;
 }
--- Study_Algorithm_180731/Study_Algorithm_180731/가장긴팰린드롬.cpp
+++ Study_Algorithm_180731/Study_Algorithm_180731/가장긴팰린드롬.cpp
@@ -12,12 +12,15 @@ void main()
 	cout << solution("abacde")<< endl;
 }
 
-
+bool IsPalindrom(string& s, bool bEven);
 
 int solution(string s) {
 	
 	size_t len = s.length();
 	string str = s;
+
+	if (0 == len)
+		return 0;
 
 	for (size_t i = 0; i < len; ++i)
 	{
@@ -25,30 +28,66 @@ int solution(string s) {
 		for (size_t j = 0; j <= i; ++j)
 		{
 			string sub = str.substr(j, len-i);
-			size_t subLen = sub.length();
-			bool bPelin = false;
 
-			for (size_t k = 1; k <= subLen / 2; ++k)
-			{
-				if (bEven)
-				{
-					if (true == (sub[subLen / 2 - k] == sub[subLen / 2 + k - 1]))
-						bPelin = true;
-					else
-						break;
-				}
-				else
-				{
-					if (true == (sub[subLen / 2 - k] == sub[subLen / 2 + k]))
-						bPelin = true;
-					else
-						break;
-				}
-			}
-			if (bPelin)
-				return subLen;
+			if (IsPalindrom(sub, bEven))
+				return sub.length();
 		}
 	}
 
 	return 1;
+}
+
+bool IsPalindrom(string & sub, bool bEven)
+{
+	size_t subLen = sub.length()/2;
+	bool bPelin = false;
+
+	for (size_t k = subLen; k >= 1; --k)
+	{
+		if (bEven)
+		{
+			if (true == (sub[subLen - k] == sub[subLen + k - 1]))
+				bPelin = true;
+			else
+			{
+				bPelin = false;
+				break;
+			}		
+		}
+		else
+		{
+			if (true == (sub[subLen - k] == sub[subLen + k]))
+				bPelin = true;
+			else
+			{
+				bPelin = false;
+				break;
+			}
+		}
+	}
+
+	return bPelin;
+}
+
+bool IsPalindrom(string & sub, bool bEven)
+{
+	string counter = sub;
+	size_t size = sub.length();
+	size_t halfSize = size / 2;
+
+	for (size_t i = 0; i<halfSize; ++i)
+		counter[i] = sub[size - i-1];
+
+	//for (size_t i = 0; i<halfSize; ++i)
+	//{
+	//	if (counter[i] != sub[i])
+	//		return false;
+	//}
+	//return true;
+
+	string half = sub.substr(0, halfSize);
+	if (0 == counter.compare(half))
+		return true;
+	else
+		return false;
 }
